aaa31455c749c09b00f2635a935254be5ca59119
sbj97 <thdqhdwo2@gmail.com>
2023-02-16 09:32:34 +0900

    2022 02 16 추가

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <h5>{{blog[0].title}}</h5>
+    <h5 @click="$router.push('/detail/0')">{{blog[0].title}}</h5>
     <p>{{blog[0].date}}</p>
   </div>
   
   <div>
-    <h5>{{blog[1].title}}</h5>
+    <h5 @click="$router.push('/detail/1')">{{blog[1].title}}</h5>
     <p>{{blog[1].date}}</p>
   </div>
   
   <div>
-    <h5>{{blog[2].title}}</h5>
+    <h5 @click="$router.push('/detail/2')">{{blog[2].title}}</h5>
     <p>{{blog[2].date}}</p>
   </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,26 @@
 import { createWebHistory, createRouter } from "vue-router";
 import BlogList from './components/BlogList.vue';
 import HomeMove from './components/HomeMove.vue';
+import DetailList from './components/DetailList.vue';
+import SbsAuthor from './components/SbsAuthor.vue';
+import SbsComment from './components/SbsComment.vue';
 const routes = [
+  
     // Page를 만들떄는 밑에만 건드리면됀다.
+  {
+    path: "/detail/:id(\\d+)",
+    component: DetailList,    
+    children: [
+      {
+      path: "author",
+      component: SbsAuthor,
+      },
+      {
+        path: " comment",
+        component: SbsComment,
+      },   
+    ]
+  },
   {
     path: "/list",
     component: BlogList,
@@ -10,7 +28,11 @@ const routes = [
   {
     path: "/",
     component: HomeMove,
-  }
+  },
+  {
+    path: "/:anything(.*)",
+    component: HomeMove,
+  },
 ];
 
 const router = createRouter({
